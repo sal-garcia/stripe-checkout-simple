400adb304a968dd36f68663d35eae36bf7f5e085
sal <salgarcia940@gmail.com>
2023-02-05 18:27:26 -0800

    commented code and moved client inside of server

--- server/server.js
+++ server/server.js
@@ -2,13 +2,14 @@ require("dotenv").config()
 
 const express = require("express")
 const app = express()
-const cors = require("cors")
+// const cors = require("cors")
 app.use(express.json())
-app.use(
-  cors({
-    origin: "http://localhost:5500",
-  })
-)
+app.use(express.static('client'))
+// app.use(
+//   cors({
+//     origin: "http://localhost:5500",
+//   })
+// )
 
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
 
@@ -17,31 +18,31 @@ const storeItems = new Map([
   [2, { priceInCents: 20000, name: "Learn CSS Today" }],
 ])
 
-app.post("/create-checkout-session", async (req, res) => {
-  try {
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      mode: "payment",
-      line_items: req.body.items.map(item => {
-        const storeItem = storeItems.get(item.id)
-        return {
-          price_data: {
-            currency: "usd",
-            product_data: {
-              name: storeItem.name,
-            },
-            unit_amount: storeItem.priceInCents,
-          },
-          quantity: item.quantity,
-        }
-      }),
-      success_url: `${process.env.CLIENT_URL}/success.html`,
-      cancel_url: `${process.env.CLIENT_URL}/cancel.html`,
-    })
-    res.json({ url: session.url })
-  } catch (e) {
-    res.status(500).json({ error: e.message })
-  }
-})
+// app.post("/create-checkout-session", async (req, res) => {
+//   try {
+//     const session = await stripe.checkout.sessions.create({
+//       payment_method_types: ["card"],
+//       mode: "payment",
+//       line_items: req.body.items.map(item => {
+//         const storeItem = storeItems.get(item.id)
+//         return {
+//           price_data: {
+//             currency: "usd",
+//             product_data: {
+//               name: storeItem.name,
+//             },
+//             unit_amount: storeItem.priceInCents,
+//           },
+//           quantity: item.quantity,
+//         }
+//       }),
+//       success_url: `${process.env.CLIENT_URL}/success.html`,
+//       cancel_url: `${process.env.CLIENT_URL}/cancel.html`,
+//     })
+//     res.json({ url: session.url })
+//   } catch (e) {
+//     res.status(500).json({ error: e.message })
+//   }
+// })
 
 app.listen(3000)
